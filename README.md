0597793aab6dc159816c1d3fb4519b0d06529da0
rhyn <rhyn@zhaw.ch>
2025-08-12 15:57:18 +0200

    Changed Script Description

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # metabarcoding-ZHAW
-A collection of scripts supporting metabarcoding workflows in the Environmental Genomics research group at ZHAW
+A collection of scripts supporting metabarcoding workflows in the Environmental Genomics research group at ZHAW. The scripts run on the ZHAW HPC Earth cluster and are not fully automated and currently still require manual configuration.
--- apscale.sh
+++ apscale.sh
@@ -5,11 +5,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
-#SBATCH --time=06:00:00
+#SBATCH --time=001:00:00
 #SBATCH --partition=earth-3
 #SBATCH --mem=20G
-#SBATCH --mail-type=end
-#SBATCH --mail-user=rhyn@zhaw.ch
 
 #rhel8module load DefaultModules
 module purge
@@ -17,13 +15,15 @@ module load DefaultModules
 module load gcc/9.4.0-pe5.34 miniconda3/4.12.0 lsfm-init-miniconda/1.0.0
 
 #=================================================================================================================================================================================
-# Script to run the apscale pipeline for the filtering, clusterin, denoising and taxonomicall assigning of metabarcoding sequencing data. 
+# Script to run the apscale pipeline for the filtering, clustering, denoising and taxonomicall assigning of metabarcoding sequencing data. 
 # Author: Nicola Rhyner, rhyn@zhaw.ch 
-# Last update: 29.07.2025
 # 
-# Check the file name renaming on line 67 before run!!
-# Example command to run the pipeline: sbatch apscale_rhyn_29112023.sh mammals_run_0039_apscale FALSE
+# To-Do before running: 
 #
+# Check the file name renaming in the function FastqRenamer and adjust if necessary (Function works with files that end in <samplename>_S**_L001_R1_001.fastq.gz)
+# Manually create an apscale project and adjust the settings in the settings file (will be automatized)
+# Store the path to your project in the variable apscale_project_path. (will be improved in future commits)
+# Store the path to your raw gzipped fastq files in the variable fastq_dir (will be improved in future commits)
 #
 #  Run the script with sbatch apscale.sh <projectname> <renaming> 
 #=================================================================================================================================================================================
@@ -32,15 +32,11 @@ module load gcc/9.4.0-pe5.34 miniconda3/4.12.0 lsfm-init-miniconda/1.0.0
 # Step 1: Define paths and variables
 #-------------------------------
 
-	#fastq_dir=/cfs/earth/scratch/rhyn/ZHAW_MiSeq_run_0071/untrimmed_raw #path to the folder containing your fastq files. Works with gzipped and gunzipped files
+	fastq_dir=/cfs/earth/scratch/rhyn/ZHAW_MiSeq_run_0071/untrimmed_raw #path to the folder containing your fastq files. Works with gzipped and gunzipped files
 	project_name=$1 #The project name of your apscale project. Must have been created prior to this script
 	rename_files=$2 #set to TRUE if your input files end with <samplename>_S**_L001_R1_001.fastq.gz or <samplename>_S**_L001_R2_001.fastq.gz Set to FALSE if they end with <samplename>_R1.fastq.gz or <samplename>_R2.fastq.gz
-    taxonomic_assignment=$3
 	apscale_env=/cfs/earth/scratch/iunr/egsb/shared/conda-envs/apscale #path to your apscale conda environment with apscale, apscale-blast and all required dependencies
     apscale_project_path=/cfs/earth/scratch/rhyn/apscale_projects #path to the folder containing your apscale project(s)
-
-# If your files have another naming then <samplename>_S**_L001_R1_001.fastq.gz, e.g. <samplename>_S**_L001_R1_001.trimmed.fastq.gz, adjust the Regex in the function FastRenamer
-
 
 #-------------------------------
 # Step 2: Define functions
@@ -85,7 +81,7 @@ conda activate apscale
 apscale --run_apscale ${apscale_project_path}/${project_name} 
 			
 #-------------------------------
-# Export a text file with all the software versions u
+# Export a text file with all the software versions 
 #-------------------------------
 #>
 #>>
@@ -93,14 +89,4 @@ apscale --run_apscale ${apscale_project_path}/${project_name}
 #>>
 #>
 		
-
-
-
-
-
-#-------------------------------
-# Deactivate Apscale 
-#-------------------------------
-	conda deactivate
-
 #%#%#%#%#%#% END #%#%#%#%#%#%#%#
